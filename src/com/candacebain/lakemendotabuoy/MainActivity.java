37f55b4843a9f2a4acc99eb2d5f7f753f506c94b
Candace Bain <candace.bain@gmail.com>
2013-07-09 22:45:04 -0500

    A start at network connectivity

--- src/com/candacebain/lakemendotabuoy/MainActivity.java
+++ src/com/candacebain/lakemendotabuoy/MainActivity.java
@@ -1,14 +1,31 @@
 package com.candacebain.lakemendotabuoy;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	// !!! Can also use XML here
+	private static final String dataQueryURL = "http://metobs.ssec.wisc.edu/app/mendota/buoy/data/json?symbols=t:rh:td:spd:dir:wt_0.0:wt_1.0:wt_5.0:wt_10.0:wt_15.0:wt_20.0";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        updateData();
         setContentView(R.layout.activity_main);
     }
 
@@ -20,4 +37,82 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    private void updateData(){
+    	// !!! I'll need to figure out how to show error messages
+    ConnectivityManager connMgr = (ConnectivityManager) 
+            getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+        	new DownloadWebpageTask().execute(dataQueryURL);
+        } else {
+            // !!!  display error
+        }
+    }
+    
+    // Uses AsyncTask to create a task away from the main UI thread. This task takes a 
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string, which is
+    // displayed in the UI by the AsyncTask's onPostExecute method.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+       @Override
+       protected String doInBackground(String... urls) {
+             
+           // params comes from the execute() call: params[0] is the url.
+           try {
+               return downloadUrl(urls[0]);
+           } catch (IOException e) {
+               return "Unable to retrieve web page. URL may be invalid.";
+           }
+       }
+       // onPostExecute displays the results of the AsyncTask.
+       @Override
+       protected void onPostExecute(String result) {
+           //textView.setText(result);
+      }
+   }
+    
+ // Given a URL, establishes an HttpUrlConnection and retrieves
+ // the web page content as a InputStream, which it returns as
+ // a string.
+ private String downloadUrl(String myurl) throws IOException {
+     InputStream is = null;
+     // Only display the first 500 characters of the retrieved
+     // web page content.
+     int len = 500;
+         
+     try {
+         URL url = new URL(myurl);
+         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+         conn.setReadTimeout(10000 /* milliseconds */);
+         conn.setConnectTimeout(15000 /* milliseconds */);
+         conn.setRequestMethod("GET");
+         conn.setDoInput(true);
+         // Starts the query
+         conn.connect();
+         int response = conn.getResponseCode();
+         Log.d("DEBUG", "The response is: " + response);
+         is = conn.getInputStream();
+
+         // Convert the InputStream into a string
+         String contentAsString = readIt(is, len);
+         return contentAsString;
+         
+     // Makes sure that the InputStream is closed after the app is
+     // finished using it.
+     } finally {
+         if (is != null) {
+             is.close();
+         } 
+     }
+ }
+ 
+//Reads an InputStream and converts it to a String.
+public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+  Reader reader = null;
+  reader = new InputStreamReader(stream, "UTF-8");        
+  char[] buffer = new char[len];
+  reader.read(buffer);
+  return new String(buffer);
+}
 }
