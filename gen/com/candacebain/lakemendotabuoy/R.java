322dc13fdff1b15a8809af92032d9f5452326f6c
Candace Bain <candace.bain@gmail.com>
2013-07-10 06:09:47 -0500

    Better formatting for table, allow connection to internet, add humidity

--- gen/com/candacebain/lakemendotabuoy/R.java
+++ gen/com/candacebain/lakemendotabuoy/R.java
@@ -24,21 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
+        public static final int action_settings=0x7f080012;
         public static final int air_temperature=0x7f080003;
         public static final int dew_point=0x7f080004;
-        public static final int fifteen_meter_temperature=0x7f08000e;
-        public static final int fifteen_meter_temperature_caption=0x7f08000d;
-        public static final int five_meter_temperature=0x7f08000a;
-        public static final int five_meter_temperature_caption=0x7f080009;
-        public static final int one_meter_temperature=0x7f080008;
-        public static final int one_meter_temperature_caption=0x7f080007;
-        public static final int surface_temperature=0x7f080006;
-        public static final int surface_temperature_caption=0x7f080005;
-        public static final int ten_meter_temperature=0x7f08000c;
-        public static final int ten_meter_temperature_caption=0x7f08000b;
-        public static final int twenty_meter_temperature=0x7f080010;
-        public static final int twenty_meter_temperature_caption=0x7f08000f;
+        public static final int fifteen_meter_temperature=0x7f08000f;
+        public static final int fifteen_meter_temperature_caption=0x7f08000e;
+        public static final int five_meter_temperature=0x7f08000b;
+        public static final int five_meter_temperature_caption=0x7f08000a;
+        public static final int humidity=0x7f080005;
+        public static final int one_meter_temperature=0x7f080009;
+        public static final int one_meter_temperature_caption=0x7f080008;
+        public static final int surface_temperature=0x7f080007;
+        public static final int surface_temperature_caption=0x7f080006;
+        public static final int ten_meter_temperature=0x7f08000d;
+        public static final int ten_meter_temperature_caption=0x7f08000c;
+        public static final int twenty_meter_temperature=0x7f080011;
+        public static final int twenty_meter_temperature_caption=0x7f080010;
         public static final int wind_direction=0x7f080000;
         public static final int wind_gust=0x7f080002;
         public static final int wind_speed=0x7f080001;
@@ -61,6 +62,8 @@ public final class R {
         public static final int fifteen_meter_temperature_caption=0x7f050016;
         public static final int five_meter_temperature=0x7f050013;
         public static final int five_meter_temperature_caption=0x7f050012;
+        public static final int humidity=0x7f05001b;
+        public static final int humidity_caption=0x7f05001a;
         public static final int one_meter_temperature=0x7f050011;
         public static final int one_meter_temperature_caption=0x7f050010;
         public static final int surface_temperature=0x7f05000f;
