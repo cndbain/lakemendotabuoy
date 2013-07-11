3e703cd5c1672097ffbad37dd655416f5bc3cef4
Candace Bain <candace.bain@gmail.com>
2013-07-10 22:46:48 -0500

    Parse XML and display data

--- src/com/candacebain/lakemendotabuoy/MainActivity.java
+++ src/com/candacebain/lakemendotabuoy/MainActivity.java
@@ -7,8 +7,12 @@ import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
 
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -24,7 +28,10 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 	private static final String DEBUG_TAG = "LakeMendotaBuoy";
-	
+
+	private static final DecimalFormat decimalFormat = new DecimalFormat(
+			"###.#");
+
 	// Do an enum map with these values?
 	private static final String AIR_TEMP = "AIR_TEMP";
 	private static final String REL_HUM = "REL_HUM";
@@ -32,16 +39,16 @@ public class MainActivity extends Activity {
 	private static final String WIND_SPEED = "WIND_SPEED_2.0";
 	private static final String WIND_DIRECTION = "WIND_DIRECTION_2.0";
 	private static final String WIND_GUST = "WIND_GUST";
-	
-	private static final String WATER_TEMP_0 = "WATER_TEMP_0";
-	private static final String WATER_TEMP_1 = "WATER_TEMP_1";
-	private static final String WATER_TEMP_5 = "WATER_TEMP_5";
-	private static final String WATER_TEMP_10 = "WATER_TEMP_10";
-	private static final String WATER_TEMP_15 = "WATER_TEMP_15";
-	private static final String WATER_TEMP_20 = "WATER_TEMP_20";
+
+	private static final String WATER_TEMP_0 = "WATER_TEMP_0.0";
+	private static final String WATER_TEMP_1 = "WATER_TEMP_1.0";
+	private static final String WATER_TEMP_5 = "WATER_TEMP_5.0";
+	private static final String WATER_TEMP_10 = "WATER_TEMP_10.0";
+	private static final String WATER_TEMP_15 = "WATER_TEMP_15.0";
+	private static final String WATER_TEMP_20 = "WATER_TEMP_20.0";
 
 	// !!! Can also use XML here
-	private static final String dataQueryURL = "http://metobs.ssec.wisc.edu/app/mendota/buoy/data/json?symbols=t:rh:td:spd:dir:gust:wt_0.0:wt_1.0:wt_5.0:wt_10.0:wt_15.0:wt_20.0";
+	private static final String dataQueryURL = "http://metobs.ssec.wisc.edu/app/mendota/buoy/data/xml?symbols=t:rh:td:spd:dir:gust:wt_0.0:wt_1.0:wt_5.0:wt_10.0:wt_15.0:wt_20.0";
 
 	private TextView windDirection;
 	private TextView windSpeed;
@@ -71,22 +78,22 @@ public class MainActivity extends Activity {
 
 		findViews();
 	}
-	
-	private void findViews(){
+
+	private void findViews() {
 		windDirection = (TextView) findViewById(R.id.wind_direction);
 		windSpeed = (TextView) findViewById(R.id.wind_speed);
 		windGust = (TextView) findViewById(R.id.wind_gust);
 		airTemperature = (TextView) findViewById(R.id.air_temperature);
 		dewPoint = (TextView) findViewById(R.id.dew_point);
 		humidity = (TextView) findViewById(R.id.humidity);
-		
+
 		surfaceTemperature = (TextView) findViewById(R.id.surface_temperature);
 		oneMeterTemperature = (TextView) findViewById(R.id.one_meter_temperature);
 		fiveMeterTemperature = (TextView) findViewById(R.id.five_meter_temperature);
 		tenMeterTemperature = (TextView) findViewById(R.id.ten_meter_temperature);
 		fifteenMeterTemperature = (TextView) findViewById(R.id.fifteen_meter_temperature);
 		twentyMeterTemperature = (TextView) findViewById(R.id.twenty_meter_temperature);
-		
+
 		oneMeterCaption = (TextView) findViewById(R.id.one_meter_temperature_caption);
 		fiveMeterCaption = (TextView) findViewById(R.id.five_meter_temperature_caption);
 		tenMeterCaption = (TextView) findViewById(R.id.ten_meter_temperature_caption);
@@ -99,6 +106,105 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private final String[] windDirections = new String[] { "NNE", "NE", "ENE",
+			"E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW",
+			"NNW" };
+
+	String getTextForWindDirectionInDegrees(double windDirectionInDegrees) {
+		if (windDirectionInDegrees <= 0 || windDirectionInDegrees > 360) {
+			// !!! resource
+			return "-";
+		}
+
+		// deal with special north case
+		if ((windDirectionInDegrees >= 348.75 && windDirectionInDegrees <= 360)
+				|| windDirectionInDegrees > 0 && windDirectionInDegrees < 11.25)
+			return "N";
+
+		double i = 11.25; // 1/2 degree increment between the directions
+		for (int j = 0; j < windDirections.length; j++) {
+			if (windDirectionInDegrees >= i
+					&& windDirectionInDegrees < (i + 22.5)) {
+				return windDirections[j];
+			}
+			i += 22.5;
+		}
+
+		// shouldn't ever get here
+		return "-";
+	}
+	
+	private void displayData(Map<String, Double> result) {
+		
+		String temperatureUnitString = "°C"; // or "°F"
+		String windSpeedUnitString = " m/s"; // or " knts"
+		String humidityUnitString = "%";
+
+		// Clear old values
+		windDirection.setText("-");
+		windSpeed.setText("-");
+		windGust.setText("-");
+		airTemperature.setText("-");
+		dewPoint.setText("-");
+		humidity.setText("-");
+		surfaceTemperature.setText("-");
+		oneMeterTemperature.setText("-");
+		fiveMeterTemperature.setText("-");
+		tenMeterTemperature.setText("-");
+		fifteenMeterTemperature.setText("-");
+		twentyMeterTemperature.setText("-");
+		
+		if (result.containsKey(WIND_DIRECTION)) {
+			double value = result.get(WIND_DIRECTION);
+			windDirection.setText(getTextForWindDirectionInDegrees(value));
+		} 
+		if (result.containsKey(WIND_SPEED)){
+			double value = result.get(WIND_SPEED);
+			windSpeed.setText(decimalFormat.format(value) + windSpeedUnitString);
+		}
+		if (result.containsKey(WIND_GUST)){
+			double value = result.get(WIND_GUST);
+			windGust.setText(decimalFormat.format(value) + windSpeedUnitString);
+		}
+		if (result.containsKey(AIR_TEMP)){
+			double value = result.get(AIR_TEMP);
+			airTemperature.setText(decimalFormat.format(value) + temperatureUnitString);
+		}
+		if (result.containsKey(DEWPOINT_CALC)){
+			double value = result.get(DEWPOINT_CALC);
+			dewPoint.setText(decimalFormat.format(value) + temperatureUnitString);
+		}
+		if (result.containsKey(REL_HUM)){
+			double value = result.get(REL_HUM);
+			humidity.setText(decimalFormat.format(value) + humidityUnitString);
+		}
+		
+		if (result.containsKey(WATER_TEMP_0)){
+			double value = result.get(WATER_TEMP_0);
+			surfaceTemperature.setText(decimalFormat.format(value) + temperatureUnitString);
+		}
+		if (result.containsKey(WATER_TEMP_1)){
+			double value = result.get(WATER_TEMP_1);
+			oneMeterTemperature.setText(decimalFormat.format(value) + temperatureUnitString);
+		}
+		if (result.containsKey(WATER_TEMP_5)){
+			double value = result.get(WATER_TEMP_5);
+			fiveMeterTemperature.setText(decimalFormat.format(value) + temperatureUnitString);
+		}
+		if (result.containsKey(WATER_TEMP_10)){
+			double value = result.get(WATER_TEMP_10);
+			tenMeterTemperature.setText(decimalFormat.format(value) + temperatureUnitString);
+		}
+		if (result.containsKey(WATER_TEMP_15)){
+			double value = result.get(WATER_TEMP_15);
+			fifteenMeterTemperature.setText(decimalFormat.format(value) + temperatureUnitString);
+		}
+		if (result.containsKey(WATER_TEMP_20)){
+			double value = result.get(WATER_TEMP_20);
+			twentyMeterTemperature.setText(decimalFormat.format(value) + temperatureUnitString);
+		}
 	}
 
 	private void updateData() {
@@ -121,21 +227,25 @@ public class MainActivity extends Activity {
 	// an InputStream. Finally, the InputStream is converted into a string,
 	// which is
 	// displayed in the UI by the AsyncTask's onPostExecute method.
-	private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+	private class DownloadWebpageTask extends
+			AsyncTask<String, Void, Map<String, Double>> {
 		@Override
-		protected String doInBackground(String... urls) {
+		protected Map<String, Double> doInBackground(String... urls) {
 
 			// params comes from the execute() call: params[0] is the url.
 			try {
 				return downloadUrl(urls[0]);
 			} catch (IOException e) {
-				return "Unable to retrieve web page. URL may be invalid.";
+				// !!! Have to figure out what to do with exceptions/error
+				// handling
+				// return "Unable to retrieve web page. URL may be invalid.";
+				return new HashMap<String, Double>();
 			}
 		}
 
 		// onPostExecute displays the results of the AsyncTask.
 		@Override
-		protected void onPostExecute(String result) {
+		protected void onPostExecute(Map<String, Double> result) {
 			displayData(result);
 		}
 	}
@@ -143,12 +253,9 @@ public class MainActivity extends Activity {
 	// Given a URL, establishes an HttpUrlConnection and retrieves
 	// the web page content as a InputStream, which it returns as
 	// a string.
-	private String downloadUrl(String myurl) throws IOException {
+	private Map<String, Double> downloadUrl(String myurl) throws IOException {
 		InputStream is = null;
-		// Only display the first 5000 characters of the retrieved
-		// web page content.
-		int len = 5000;
-
+		Map<String, Double> entries = new HashMap<String, Double>();
 		try {
 			URL url = new URL(myurl);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -161,35 +268,43 @@ public class MainActivity extends Activity {
 			int response = conn.getResponseCode();
 			Log.d(DEBUG_TAG, "The response is: " + response);
 			is = conn.getInputStream();
-			
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(is, null);
-            parser.nextTag();
-            
-            List entries = new ArrayList();
 
-            parser.require(XmlPullParser.START_TAG, ns, "feed");
-            while (parser.next() != XmlPullParser.END_TAG) {
-                if (parser.getEventType() != XmlPullParser.START_TAG) {
-                    continue;
-                }
-                String name = parser.getName();
-                // Starts by looking for the entry tag
-                if (name.equals("entry")) {
-                    entries.add(readEntry(parser));
-                } else {
-                    skip(parser);
-                }
-            }  
-            return entries;
+			XmlPullParser parser = Xml.newPullParser();
+			parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+			parser.setInput(is, null);
+			parser.nextTag();
+
+			String dataType = null;
+			
+			parser.require(XmlPullParser.START_TAG, null, "metobs");
+			while (parser.next() != XmlPullParser.END_DOCUMENT) {
+				if (parser.getEventType() == XmlPullParser.START_TAG) {
+					String name = parser.getName();
+					if (name.equals("data")){
+						dataType = parser.getAttributeValue(null, "symbol");
+					}
+				}
+				else if (parser.getEventType() == XmlPullParser.TEXT && dataType != null){
+					// Get the value and store it
+					String text = parser.getText();
+					String [] values = text.split(",");
+					entries.put(dataType, Double.parseDouble(values[values.length - 1]));
+				}
+				else if (parser.getEventType() == XmlPullParser.END_TAG){
+					dataType = null;
+				}
+			}
 			// Makes sure that the InputStream is closed after the app is
 			// finished using it.
+		} catch (XmlPullParserException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		} finally {
 			if (is != null) {
 				is.close();
 			}
 		}
+		return entries;
 	}
 
 	// Reads an InputStream and converts it to a String.
